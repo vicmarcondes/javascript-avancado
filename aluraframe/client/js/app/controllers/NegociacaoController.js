c4c77347578988962f1d6cf23a15d52eee44ec27
Victor Moreno <victorgalves@hotmail.com>
2019-05-02 17:02:59 -0300

    Exibe a mensagem de sucesso

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,19 +5,24 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+       
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView();
-        this._mensagem = new Mensagem();
-
         this._negociacoesView.update(this._listaNegociacoes);
+       
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
-        event.preventDefault(); 
-        
+        event.preventDefault();         
         this._listaNegociacoes.adiciona(this._criaNegociacao());  
-        this._negociacoesView.update(this._listaNegociacoes);  
+        this._negociacoesView.update(this._listaNegociacoes);
+
         this._mensagem.texto = "Negociação adicionada com sucesso!";
+        this._mensagemView.update(this._mensagem);
+        
         this._limpaFormulario();    
     }
 
