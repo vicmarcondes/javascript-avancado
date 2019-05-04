a3eb9695212018bacd69dc031db8631bfa8425f5
Victor Moreno <victorgalves@hotmail.com>
2019-05-04 09:50:30 -0300

    Isola o código do Proxy no ProxyFactory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,22 +7,13 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
        
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{ // cria um proxy (objeto idêntico) para colocar as traps
-            get(target, prop, receiver) { // todo método em JS para por um getter
-                if(["adiciona","esvazia"].includes(prop) && typeof(target[prop]) == typeof(Function)){ // se conter o método faz..
-                    return function() {
-                        console.log(`A prop ${prop} foi chamada`); // imprime o nome do método                        
-                        Reflect.apply(target[prop], target, arguments); // (chama o método, passa a referência dele mesmo, argumentos)
-                        self._negociacoesView.update(target); // faz o update da view
-                    }
-                }
-                return Reflect.get(target, prop, receiver); // se o método não for os escolhidos, apenas faça o get padrão
-            }
-        });
+        this._listaNegociacoes = ProxyFactory
+            .create(new ListaNegociacoes(), ["adiciona", "esvazia"], model => this._negociacoesView.update(model)); // cria um proxy (objeto idêntico) para colocar as traps     
         this._negociacoesView = new NegociacoesView($('#tabela'));
         this._negociacoesView.update(this._listaNegociacoes);
        
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory
+            .create(new Mensagem(), ["texto"], model => this._mensagemView.update(model));
         this._mensagemView = new MensagemView($("#mensagemView"));
         this._mensagemView.update(this._mensagem);
     }
@@ -30,18 +21,13 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();         
         this._listaNegociacoes.adiciona(this._criaNegociacao());  
-
-        this._mensagem.texto = "Negociação adicionada com sucesso!";
-        this._mensagemView.update(this._mensagem);
-        
+        this._mensagem.texto = "Negociação adicionada com sucesso!";    
         this._limpaFormulario();    
     }
 
     esvazia() {
         this._listaNegociacoes.esvazia();
-
         this._mensagem.texto = "A lista de negociações foi esvaziada!";
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
